299169fc98da8a0440fc99d301a8f34df0ff304f
KinIcy NET <nkinicy@gmail.com>
2018-03-01 15:58:25 -0500

    fixed HTML not being parsed

--- components/SpaceView.vue
+++ components/SpaceView.vue
@@ -9,7 +9,7 @@
           <b-list-group-item  v-for="post in posts" :key="post.pid">
             <b-media class="text-break">
               <b-img slot="aside" rounded="circle" :src="'/images/users/small/'+ post.user_msg + '.jpg'" />
-              <strong>{{post.user_msg}}</strong> {{renderMessage(post.msg)}}
+              <strong>{{post.user_msg}}</strong> <span v-html="renderMessage(post.msg)"></span>
             </b-media>
           </b-list-group-item>
           <b-list-group-item v-if="posts.length === 0">
@@ -40,6 +40,9 @@ export default {
     },
     renderMessage (message) {
       return decodeURI(message)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt')
+        .replace(/"/g, '&quot')
         .replace(/(https?:\/\/[^\s]+)/g, url => `<a href="${url}">${url}</a>`)
         .replace(/\n/g, '<br>')
     }
