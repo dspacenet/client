04a1df542ba114687d2ae57442dc6ea2fc1191e5
Sirquini <sirquini2@gmail.com>
2018-03-23 10:22:07 -0500

    Updated footer

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -2,7 +2,7 @@
   <footer class="sticky-footer">
     <div class="container">
       <div class="text-center">
-        <small>By AVISPA Research Group and Ecole Polytechnique. Héctor Delgado, Jason Lopez, Camilo Rueda and Frank Valencia</small>
+        <small>By AVISPA Research Group and Ecole Polytechnique. Héctor Delgado, Jason Lopez, Santiago Quintero, Camilo Rueda and Frank Valencia</small>
       </div>
     </div>
   </footer>
