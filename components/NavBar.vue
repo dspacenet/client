335a0ee0cedf76b1acfe7dcd7324553b702916e6
KinIcy NET <nkinicy@gmail.com>
2018-07-03 16:26:05 +0200

    initial implementation of notifications

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -7,6 +7,29 @@
     <b-navbar-toggle target="navbarResponsive"/>
     <b-collapse is-nav id="navbarResponsive">
       <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown class="mr-2">
+          <template slot="button-content">
+          <icon name="bell"/>
+          <span class="d-lg-none">
+            Alerts
+            <b-badge pill variant="warning" v-if="notifications.length">{{notifications.length}}</b-badge>
+          </span>
+          <span class="indicator text-warning d-none d-lg-block">
+            <icon name="circle" scale="0.6"/>
+          </span>
+          </template>
+          <b-dropdown-header>Notifications</b-dropdown-header>
+          <template v-for="notification in notifications">
+          <b-dropdown-divider :key="notification.clock" />
+          <b-dropdown-item :key="notification.clock">
+            <span class="text-success">
+              <strong><icon name="info-circle" class="mx-1" />{{notification.user_msg}}</strong>
+            </span>
+            <span class="small float-right text-muted">{{notification.clock}}</span>
+            <div class="dropdown-message small">{{notification.msg}}.</div>
+          </b-dropdown-item>
+          </template>
+        </b-nav-item-dropdown>
         <b-nav-item :to="'/space/' + username">
           <b-img rounded="circle" width="22" height="22" :src="'/images/' + username + '.jpg'" />
           {{username}} ({{userId}})
@@ -32,13 +55,29 @@
 import 'vue-awesome/icons/question-circle'
 import 'vue-awesome/icons/binoculars'
 import 'vue-awesome/icons/sign-out'
+import 'vue-awesome/icons/bell'
+import 'vue-awesome/icons/circle'
+import 'vue-awesome/icons/info-circle'
 
 export default {
   data () {
     return {
       username: this.$auth.state.user ? this.$auth.state.user.name : '',
-      userId: this.$auth.state.user ? this.$auth.state.user.id : ''
+      userId: this.$auth.state.user ? this.$auth.state.user.id : '',
+      notifications: []
     }
+  },
+  methods: {
+    async getNotifications () {
+      try {
+        this.notifications = await this.$axios.$get(`space/${this.userId}.12`)
+      } catch (error) {
+        this.error = error.response ? error.response.error : error.message
+      }
+    }
+  },
+  mounted () {
+    this.getNotifications()
   }
 }
 </script>
@@ -46,5 +85,16 @@ export default {
 <style>
 .bg-dark-transparent {
   background-color: rgba(52,58,64,.95) !important;
+}
+
+.indicator {
+  position: absolute;
+  top: -3px;
+  left: 21px;
+}
+
+.dropdown-message {
+  max-width: 250px;
+  overflow: hidden;
 }
 </style>
