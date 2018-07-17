1597cdfa5ed8f6df9813ff34099bf22f898b72af
KinIcy NET <nkinicy@gmail.com>
2018-07-17 11:07:37 +0200

    fixing naming changes

--- components/SpaceView.vue
+++ components/SpaceView.vue
@@ -8,8 +8,8 @@
         <template v-if="!isLoading">
           <b-list-group-item  v-for="post in posts" :key="post.pid">
             <b-media class="text-break">
-              <b-img slot="aside" rounded="circle" :src="'/images/users/small/'+ post.user_msg + '.jpg'" />
-              <strong>{{post.user_msg}}&nbsp;</strong> <span v-html="renderMessage(post.msg)"></span>
+              <b-img slot="aside" rounded="circle" :src="'/images/users/small/'+ post.user + '.jpg'" />
+              <strong>{{post.user}}&nbsp;</strong> <span v-html="renderMessage(post.content)"></span>
             </b-media>
           </b-list-group-item>
           <b-list-group-item v-if="posts.length === 0">
